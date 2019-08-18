cc2724a8b70073afadb9dad94eed465755c36bad
huletanvi <54231078+huletanvi@users.noreply.github.com>
2019-08-19 01:12:12 +0530

    Add files via upload
    initial commit

---
+++ project/assets/js/new.js
@@ -0,0 +1,11 @@
+var bar = new ProgressBar.SemiCircle(container, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: null
+});
+
+bar.animate(1.0);
---
+++ project/assets/js/progressbar.js
@@ -0,0 +1,7 @@
+<div class="progress-circle p10">
+  <span>10%</span>
+  <div class="left-half-clipper">
+    <div class="first50-bar"></div>
+    <div class="value-bar"></div>
+  </div>
+</div>
